4a643fa1d19264c17934e1ee686965ddaf9de913
Damar Jati <damarcodes@gmail.com>
2024-12-14 21:36:44 +0700

    Create freetw.py

---
+++ freetw.py
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Free TW Like</title>
+    <style>
+        /* Reset margin dan padding untuk memastikan iframe memenuhi layar */
+        html, body {
+            margin: 0;
+            padding: 0;
+            height: 100%;
+            overflow: hidden;
+        }
+
+        /* Iframe tanpa border dan memenuhi layar */
+        iframe {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            border: none; /* Menghapus border */
+        }
+    </style>
+</head>
+<body>
+    <!-- Ganti src dengan URL yang ingin Anda tampilkan -->
+    <iframe src="https://damarjati-freetw.hf.space/?__theme=dark&__sign=eyJhbGciOiJFZERTQSJ9.eyJyZWFkIjp0cnVlLCJwZXJtaXNzaW9ucyI6eyJyZXBvLmNvbnRlbnQucmVhZCI6dHJ1ZX0sIm9uQmVoYWxmT2YiOnsia2luZCI6InVzZXIiLCJfaWQiOiI2NDU2ZjNjYTFjYTlkZWJhYjA1NTRmOGIiLCJ1c2VyIjoiRGFtYXJKYXRpIn0sImlhdCI6MTczNDE4NjkwMywic3ViIjoiL3NwYWNlcy9EYW1hckphdGkvRnJlZXR3IiwiZXhwIjoxNzM0MjczMzAzLCJpc3MiOiJodHRwczovL2h1Z2dpbmdmYWNlLmNvIn0.7v0ATLtYiOdT_oscPf7dIm4EAg-DKTcP8j7TWXf26FWbs4pcX9MZ1yp7l52aBewyyHUQAIy-3M6RoncJyDYODA"></iframe>
+</body>
+</html>
